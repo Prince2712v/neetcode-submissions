18a10016d251267a68856a0b00efad57f803b12f
Prince2712v <princesinghramwat@gmail.com>
2026-07-04 10:29:11 +0530

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] arr, int target) {
+        int []brr = new int[2];
+        for(int i =0;i<arr.length;i++){
+            for(int j=i+1; j<arr.length;j++){
+                if(arr[i]+arr[j] == target){
+                    brr[0] = i;
+                    brr[1] = j;
+                    break ;
+                }
+            }
+        }
+        return brr ;
+    }
+}
